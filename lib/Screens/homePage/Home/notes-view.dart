75e570da7026d08775ed3b048cd70190533637b4
HazemHamdy7 <hazemhamdy1215@gmail.com>
2023-04-16 15:34:59 +0200

    create fetch notes method

--- lib/Screens/homePage/Home/notes-view.dart
+++ lib/Screens/homePage/Home/notes-view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_app_hive/Screens/homePage/widgets/notes_view_body.dart';
+import 'package:note_app_hive/cubits/notes_cubit/notes_cubit_cubit.dart';
 
 import '../widgets/add_bottom_model_sheet.dart';
 
@@ -9,22 +11,25 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                context: context,
-                builder: (context) {
-                  return const AddNoteBottomSheet();
-                });
-          },
-          child: const Icon(Icons.add),
+      child: BlocProvider(
+        create: (context) => NotesCubit(),
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return const AddNoteBottomSheet();
+                  });
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: const NotesViewBody(),
         ),
-        body: const NotesViewBody(),
       ),
     );
   }
--- lib/cubits/notes_cubit/notes_cubit_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
+import '../../core/constant.dart';
 import '../../models/note_model.dart';
 
 part 'notes_cubit_state.dart';
 
-class NotesCubitCubit extends Cubit<NotesCubitState> {
-  NotesCubitCubit() : super(NotesCubitInitial());
+class NotesCubit extends Cubit<NotesCubitState> {
+  NotesCubit() : super(NotesCubitInitial());
+
+  fetchAllNotes() {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesCubitSuccess(notes));
+    } catch (e) {
+      emit(NotesCubitFailure(e.toString()));
+    }
+  }
 }
